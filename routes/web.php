867e884cfaf9a28a5c959d2f9ba216f488f49490
Michael Raheem <abiolar544@gmail.com>
2022-08-20 04:28:04 +0100

    redirect home:

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,7 @@ use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MessageController::class, 'index']);
 
 Auth::routes();
 
